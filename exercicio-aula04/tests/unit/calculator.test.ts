d3f368446cc8d13dc799d95dad0007cc2199e5dc
gabriela <gabrielamontanhini@gmail.com>
2023-10-04 18:09:46 -0300

    Resolução 04

--- exercicio-aula04/tests/unit/calculator.test.ts
+++ exercicio-aula04/tests/unit/calculator.test.ts
@@ -1,5 +1,24 @@
+import calculator from "calculator";
+
 describe("calculator tests", () => {
-  it("should work", async () => {
-    expect(true).toBe(true);
+  it("should sum two numbers", async () => {
+    const result = calculator.sum(2, 3)
+    expect(result).toBe(5);
   });
+  it("should subtract two numbers",async () => {
+    const result = calculator.sub(10, 1)
+    expect(result).toBe(9)
+  })
+  it("should multiply two numbers", async () => {
+    const result = calculator.mul(3, 1)
+    expect(result).toBe(3)  
+  })
+  it("should divide two numbers",async () => {
+    const result = calculator.div(6, 2)
+    expect(result).toBe(3)
+  })
+  it("should return 0 when divided by zero", async () => {
+    const result = calculator.div(1000, 0)
+    expect(result).toBe(0)
+  })
 })
