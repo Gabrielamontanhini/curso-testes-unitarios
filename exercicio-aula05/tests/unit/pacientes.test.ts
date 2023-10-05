59361cebb98c4d22b6b7aee41e8aa02d182d65f5
gabriela <gabrielamontanhini@gmail.com>
2023-10-05 14:11:16 -0300

    EX 05

--- exercicio-aula05/tests/unit/pacientes.test.ts
+++ exercicio-aula05/tests/unit/pacientes.test.ts
@@ -1,5 +1,25 @@
-describe("calculator tests", () => {
-  it("should work", async () => {
-    expect(true).toBe(true);
+import { faker } from "@faker-js/faker";
+import { generateProtocolForPacient } from "../../src/protocols-generator";
+
+jest.mock("uuid", ()=>{
+  return {
+    v4: () => {
+      return "mocked"
+    }
+  }
+})
+
+describe("Protocol generator", () => {
+  it("It should create a protocol", async () => {
+    const firstName = faker.person.firstName()
+    const lastName = faker.person.lastName()
+    const priority = faker.datatype.boolean()
+    const protocol = generateProtocolForPacient(firstName, lastName, priority)
+    expect(protocol).toEqual({
+      priority,
+      date: expect.any(Date),
+      pacient: `${firstName} ${lastName}`,
+      protocol: "mocked"
+    });
   });
 });
